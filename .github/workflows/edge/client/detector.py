504e80fa545a91496a74bdd35d182045f833a93f
Adriana Monteiro Martani <80915974+adrianamartani@users.noreply.github.com>
2025-11-11 13:57:02 -0300

    Update comments in detector.py for clarity

--- .github/workflows/edge/client/detector.py
+++ .github/workflows/edge/client/detector.py
@@ -1,7 +1,7 @@
-# detector.py - stub que simula uma detecção
+# detector.py - simula uma detecção
 import random
 def detect(image_path=None):
-    # retorna formato: [{"class":"capacete","conf":0.9, "bbox":[x1,y1,x2,y2]}]
+    # formato: [{"class":"capacete","conf":0.9, "bbox":[x1,y1,x2,y2]}]
     if random.random() > 0.5:
         return [{"class":"capacete","conf":0.95,"bbox":[10,10,100,100]}]
     else:
